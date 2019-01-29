4d08afc978467735df6371e2cb40239e0acaf4c7
Nobay <azzadsaadeddine.sma@gmail.com>
2019-01-29 22:49:14 +0100

    fixed an issue related to routing ( existing user )

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,31 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NativeStorage} from '@ionic-native/native-storage/ngx';
 import {LoadingController} from '@ionic/angular';
-import {Router} from '@angular/router';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import {GooglePlus} from '@ionic-native/google-plus/ngx';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage implements OnInit, OnDestroy {
 
     user: any;
     userReady = false;
+    subscription: Subscription;
 
     constructor(
         private googlePlus: GooglePlus,
         private nativeStorage: NativeStorage,
         public loadingController: LoadingController,
         private router: Router
-    ) { }
+    ) {
+        this.subscription = this.router.events.subscribe((e: any) => {
+            if (e instanceof NavigationEnd) {
+                this.fetchUser();
+            }
+        });
+    }
 
     async ngOnInit() {
+    }
+    async fetchUser() {
         const loading = await this.loadingController.create({
             message: 'Please wait...'
         });
         await loading.present();
+        console.log(this.nativeStorage.getItem('google_user'));
         this.nativeStorage.getItem('google_user')
             .then(data => {
                 this.user = {
@@ -51,10 +62,13 @@ export class ProfilePage implements OnInit {
                 this.router.navigate(['/']);
             }, err => {
                 console.error('Error logging out from Google: ' + err);
-                this.googlePlus.trySilentLogin({'webClientId': '39902291526-ue6a0t28dniimm132gj8vt26v9ej1236.apps.googleusercontent.com'}).then(res => {
+                this.googlePlus.trySilentLogin({
+                    'webClientId': '39902291526-ue6a0t28dniimm132gj8vt26v9ej1236.apps.googleusercontent.com'
+                }).then(res => {
                     console.error('Google trySilentLogin success');
-                    this.googlePlus.logout().then( obj => {
+                    this.googlePlus.disconnect().then( obj => {
                         console.error('Google logout success');
+                        console.log(this.nativeStorage.getItem('google_user'));
                         this.nativeStorage.remove('google_user');
                         this.router.navigate(['/']);
                         }, err2 => {
@@ -65,6 +79,12 @@ export class ProfilePage implements OnInit {
                     console.error('Google trySilentLogin error: ' + err);
                 });
             });
+    }
+
+    ngOnDestroy(): void {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
     }
 
 }
