d1359acedd6bb800a6654294cd5060d31084e80e
Nobay <azzadsaadeddine.sma@gmail.com>
2019-02-06 21:32:23 +0100

    added speech recognition

--- src/app/authentication/authentication.page.ts
+++ src/app/authentication/authentication.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {AuthServiceProvider} from '../providers/auth-service.provider';
 import {SpeechServiceProvider} from '../providers/speech-service.provider';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-authentication',
@@ -11,10 +12,27 @@ export class AuthenticationPage {
 
     user: any;
     error: string;
+    loginMatches: string[] = [
+        'login', 'sign-in', 'sign in', 'login with google',
+        'login google', 'google login', 'google sign-in',
+        'google sign in', 'sign-in with google', 'sign in with google',
+        'sign in google', 'sign-in google', 'authentication',
+        'authenticate', 'authenticate with google', 'google authentication',
+        'google authenticate'
+    ];
+    createMatches: string[] = [
+        'create', 'sign-up', 'sign up', 'create account',
+        'account creation', 'create account page', 'account creation page',
+        'sign-up page', 'sign up page', 'registration',
+        'registration page', 'register', 'account registration',
+        'account registration page', 'new account', 'new account page',
+        'new account creation', 'create new account'
+    ];
 
   constructor(
       private authService: AuthServiceProvider,
-      private speechService: SpeechServiceProvider
+      private speechService: SpeechServiceProvider,
+      private router: Router
   ) {
       this.user = {
           email: '',
@@ -22,20 +40,42 @@ export class AuthenticationPage {
       };
   }
 
-    async doGoogleLogin() {
+    doGoogleLogin() {
         this.authService.googleLogin(this.error);
     }
 
-    async doNormalLogin() {
+    doNormalLogin() {
         this.authService.normalLogin(this.user, this.error, undefined);
     }
 
     activateSpeech() {
       this.speechService.getPermission();
-      this.speechService.startListening();
+      this.speechService.startListening().subscribe( matches => {
+          console.log(matches);
+          this.loginBySpeech(matches);
+          this.createBySpeech(matches);
+          });
     }
 
-    stopSpeech() {
-        this.speechService.stopListening();
+    loginBySpeech(matches: string[]) {
+        for ( let i = 0; i < matches.length; i++) {
+            for ( let j = 0; j < this.loginMatches.length; j++) {
+                if (matches[i].toLowerCase() === this.loginMatches[j]) {
+                    this.doGoogleLogin();
+                    return;
+                }
+            }
+        }
+    }
+
+    createBySpeech(matches: string[]) {
+        for ( let i = 0; i < matches.length; i++) {
+            for ( let j = 0; j < this.createMatches.length; j++) {
+                if (matches[i].toLowerCase() === this.createMatches[j]) {
+                    this.router.navigate(['/signup']);
+                    return;
+                }
+            }
+        }
     }
 }
--- src/app/providers/speech-service.provider.ts
+++ src/app/providers/speech-service.provider.ts
@@ -2,17 +2,11 @@ import {Injectable} from '@angular/core';
 import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
 @Injectable()
 export class SpeechServiceProvider {
-    matches: String[];
+    matches: string[];
     isRecording = false;
     constructor(
         private speechRecognition: SpeechRecognition
     ) {}
-
-    stopListening() {
-        this.speechRecognition.stopListening().then(() => {
-            this.isRecording = false;
-        });
-    }
 
     getPermission() {
         this.speechRecognition.hasPermission()
@@ -30,10 +24,7 @@ export class SpeechServiceProvider {
         const options = {
             language: 'en-US'
         };
-        this.speechRecognition.startListening(options).subscribe(matches => {
-            this.matches = matches;
-        });
-        this.isRecording = true;
+        return this.speechRecognition.startListening(options);
     }
 
 }
