52151931684e2ed61cf4d2738567a4e70448908c
unknown <achraf.taitai1@gmail.com>
2019-03-10 03:05:33 +0100

    fix back button issue while creating a new item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
     { path: '', loadChildren: './authentication/authentication.module#AuthenticationPageModule' },
-    /* { path: 'todo', loadChildren: './todo/todo.module#TodoPageModule' }, */
+     { path: 'todo', loadChildren: './todo/todo.module#TodoPageModule' },
     { path: 'todo-item', loadChildren: './todo-item/todo-item.module#TodoItemPageModule' },
     { path: 'create-item', loadChildren: './todo-item/create-item/create-item.module#CreateItemPageModule' },
    /*  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }, */
